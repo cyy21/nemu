87140cea58376b3da111e481b6a599e9298b1548
cyy <374996681@qq.com>
2018-09-05 20:55:43 -0400

    first commit

---
+++ nemu/src/monitor/debug/ui.c
@@ -0,0 +1,154 @@
+#include "monitor/monitor.h"
+#include "monitor/expr.h"
+#include "monitor/watchpoint.h"
+#include "nemu.h"
+
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void cpu_exec(uint64_t);
+
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(nemu) ");
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
+
+static int cmd_c(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
+static int cmd_q(char *args) {
+  return -1;
+}
+
+static int cmd_help(char *args);
+
+static int cmd_si(char *args){
+	if(args==NULL){
+		cpu_exec(1);
+		return 0;
+	}
+	char *val=strtok(args," ");
+	int turns=atoi(val);
+	cpu_exec(turns);
+	return 0;
+}
+static void dump_regs()
+{
+	int i;
+	for(i=R_EAX;i<=R_EDI;++i){
+		printf("%s: 0x%08x\n",regsl[i],cpu.gpr[i]._32);
+	}
+	printf("eip: 0x%08x\n",cpu.eip);
+}
+static int cmd_dump_mem(char *args) {
+   printf("ok"); 
+
+    return 0;
+}
+
+static int cmd_print(char *args)
+{
+	if(args==NULL){
+		printf("wrong cmd");
+		return -1;
+	}
+	switch(args[0]){
+		case 'r':dump_regs();return 0;
+		case 'w':return 1;
+	}
+	return 1;
+}
+static struct {
+  char *name;
+  char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display informations about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+  {"si","step instruction",cmd_si },
+  {"info","print process condition",cmd_print},
+  {"x","dump memory",cmd_dump_mem},
+  /* TODO: Add more commands */
+
+};
+
+#define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+void ui_mainloop(int is_batch_mode) {
+  if (is_batch_mode) {
+    cmd_c(NULL);
+    return;
+  }
+
+  while (1) {
+    char *str = rl_gets();
+    char *str_end = str + strlen(str);
+
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) { continue; }
+
+    /* treat the remaining string as the arguments,
+     * which may need further parsing
+     */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
+
+#ifdef HAS_IOE
+    extern void sdl_clear_event_queue(void);
+    sdl_clear_event_queue();
+#endif
+
+    int i;
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) { return; }
+        break;
+      }
+    }
+
+    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+  }
+}
